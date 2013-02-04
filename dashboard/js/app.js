5657ce15778943a2b4eab7a4cfa0f08e200d2f9a
James Yu <james@parse.com>
2013-02-03 16:18:42 -0800

    dashboard now pulls in and displays all info

--- dashboard/js/app.js
+++ dashboard/js/app.js
@@ -1,5 +1,81 @@
-// Parse backed class to get test data
-var Test = Parse.Object.extend("Test");
+// Parse backed class to get the overall test data
+var Test = Parse.Object.extend({
+  className: "Test",
+
+  // The cohort keys
+  cohortKeys: function() {
+    return _(this.attributes).keys().filter(function(o) {
+      return /^cohort_/.test(o);
+    });
+  },
+
+  // The event keys
+  eventKeys: function() {
+    return _(this.attributes).chain().keys().filter(function(o) {
+      return /^event_/.test(o);
+    }).sortBy(function(key) {
+      return key
+    }).values();
+  },
+
+  // The total number of events in the test
+  totalEvents: function() {
+    var self = this;
+    return this.eventKeys().reduce(function(memo, eventKey) {
+      return memo + self.get(eventKey);
+    }, 0);
+  },
+
+  // Iterates and pulls out all event info into an object
+  // format is:
+  //    event_EVENTNAME___COHORTNAME
+  events: function() {
+    var self = this;
+    var events = {};
+
+    this.eventKeys().each(function(eventKey) {
+      // Populate an object with all event info in an easily
+      // traversable structure
+      var matches = eventKey.match(/^event_(.*)___(.*)$/);
+      if (matches) {
+        var eventName = matches[1];
+        var cohortName = matches[2];
+        events[eventName] = events[eventName] || {};
+        events[eventName][cohortName] = self.get(eventKey);
+      }
+    });
+
+    return events;
+  }
+});
+
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    "test/:name": "test"
+  },
+
+  initialize: function(options) {
+    this.state = options.state;
+  },
+
+  test: function(name) {
+    var self = this;
+    var loadTest = function() {
+      var test = self.state.get('tests').find(function(item) { return item.get('name') === name });
+      
+      if (test) {
+        self.state.set('dashboardTest', test);
+      }
+    };
+    
+    if (this.state.get('loading')) {
+      this.state.on('change:loading', loadTest);
+    } else {
+      loadTest();
+    }
+  }
+});
+
 
 // Holds all the state of the app
 var AppState = Backbone.Model.extend({
@@ -7,7 +83,8 @@ var AppState = Backbone.Model.extend({
   dashboardTest: null,
   
   initialize: function() {
-    this.set("tests", new Backbone.Collection());
+    this.set("tests", new Parse.Collection());
+    this.set('loading', true);
     this.fetchTests();
   },
 
@@ -20,6 +97,7 @@ var AppState = Backbone.Model.extend({
     query.find({
       success: function(results) {
         self.get('tests').reset(results);
+        self.set('loading', false);
       }
     });
   },
@@ -40,21 +118,38 @@ var Menu = Backbone.View.extend({
   },
 
   render: function() {
-    console.log(this.state.get('tests'));
+    var template = $("#menu_template").html();
+    var html = _.template(template)({ tests: this.state.get('tests') });
+    
+    this.$el.html(html);
   }
 });
 
 // Dashboard view (right main view)
 var Dashboard = Backbone.View.extend({
-  el: "#dashboard"
+  el: "#dashboard",
+
+  initialize: function(options) {
+    _.bindAll(this, 'render');
+    this.state = options.state;
+    this.state.on('change:dashboardTest', this.render);
+  },
+
+  render: function() {
+    var template = $("#dashboard_template").html();
+    var html = _.template(template)({ test: this.state.get('dashboardTest') });
+    this.$el.html(html);
+  }
 });
 
 // The main app entry point
 var App = {
   initialize: function() {
-    window.state = state = new AppState({
+    var state = new AppState({
       
     });
+
+    new AppRouter({ state: state });
 
     new Dashboard({
       state: state
@@ -63,6 +158,8 @@ var App = {
     new Menu({
       state: state
     });
+
+    Backbone.history.start();
   }
 }
 
